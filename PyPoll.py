b7328e1b0ad0ef4761957527e5531dbb48b8579f
Haley Miller <haleyxmiller@gmail.com>
2022-03-06 11:47:10 -0500

    Adding county information to election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -105,10 +105,10 @@ with open(file_to_save,"w") as txt_file:
             winning_candidate=candidate_name
         # Print winning candidate, vote count, and percentage 
     winning_candidate_summary=(
-                f"------------\n"
+                f"-------------------------\n"
                 f"Winner: {winning_candidate}\n"                    
                 f"Winning Vote Count: {winning_count:,}\n"
-                f"Winning Percentage: {winning_percentage:,}"
-                f"-----------\n")
+                f"Winning Percentage: {winning_percentage:.1f}\n"
+                f"-------------------------\n")
     print(winning_candidate_summary)
     txt_file.write(winning_candidate_summary)
